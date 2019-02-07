581880b95ac641b6db20ffa465e1eee32d9e98bd
qinshuang1998 <1378860132@qq.com>
2019-02-07 17:50:33 +0800

    Site updated: 2019-02-07 17:50:32

--- js/head.js
+++ js/head.js
@@ -24,7 +24,7 @@ $(function () {
     }
   })
 })
-(function(){
+$(function(){
     var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
     if (curProtocol === 'https') {
@@ -35,4 +35,4 @@ $(function () {
     }
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
-})();
+})
